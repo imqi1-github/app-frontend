68a4d79f61efbf94f580b59aad2e88e7bfae2202
棋 <imqi1@qq.com>
2025-02-05 22:52:27 +0800

    新增404页面、用户登录

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import {onMounted} from "vue";
+
+onMounted(() => {
+  console.log(`项目渲染完成，后端 API 地址为：${import.meta.env.VITE_API_URL}`);
+})
+
 </script>
 <template>
   <div class="row-start-1 px-32 py-2 flex justify-center border-b-gray-300 border-b-1">
