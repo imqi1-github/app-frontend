4d74177db21522b3c2944647ce6e52eccef450e6
棋 <imqi1@qq.com>
2025-02-05 22:41:14 +0800

    新增404页面、用户登录

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,19 +13,23 @@ const routes = [
   {
     "name": "登录",
     "path": "/login",
+  },
+  {
+    "name": "访问ImQi1 <i class='ri-sm ri-external-link-line'></i>",
+    "path": "https://imqi1.com"
   }
 ]
 </script>
 
 <template>
-  <div class="flex justify-between w-full">
+  <div class="flex items-center justify-between w-full">
     <RouterLink class="flex items-center justify-center" :to="$router.resolve({ name: 'home' }).href">
-      <img width="25" height="25" :src="Logo" alt="logo"/>
+      <img width="24" height="24" :src="Logo" alt="logo"/>
       <span class="font-bold text-gray-600 ml-2">本地宝</span>
     </RouterLink>
-    <div class="flex items-center justify-between gap-8">
-      <RouterLink class="hover:text-blue-600" v-for="route of routes" :to="route.path" active-class="text-blue-600">
-        {{ route.name }}
+    <div class="flex items-baseline justify-between gap-6">
+      <RouterLink class="hover:text-blue-600" v-for="route of routes" :to="route.path" active-class="text-blue-600"
+                  v-html="route.name">
       </RouterLink>
     </div>
   </div>
